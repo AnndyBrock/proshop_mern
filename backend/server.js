704ed5165d32e0dfc5cdd071c169edd5c88de572
atroian <andrii.troian@corezoid.com>
2021-06-13 21:20:14 +0300

    created routes get request for products

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,20 @@
 import express from 'express';
 import cconnectBD from './config/db.js';
 import dotenv from 'dotenv';
+import productRoutes from './routes/productRoutest.js'
+import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 import colors from 'colors';
-import products from './data/products.js';
 
 dotenv.config();
 const app = express();
 cconnectBD();
+
+app.use('/api/products', productRoutes);
+
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 const port = process.env.PORT || 5000;
 
@@ -16,11 +24,3 @@ app.get('/', (req, res)=>{
     res.send('API running')
 });
 
-app.get('/api/products', (req, res)=>{
-    res.json(products)
-});
-
-app.get('/api/products/:id', (req, res)=>{
-    const product = products.find(p => p._id === req.params.id);
-    res.json(product )
-});
