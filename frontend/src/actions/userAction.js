f510d5ebec3a708f2f6a4eb8a415c176717a6796
atroian <andrii.troian@corezoid.com>
2021-06-27 22:39:43 +0300

    register form. register user action and reducer

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { USER_LOGIN_FAIL, USER_LOGIN_SUCCESS, USER_LOGIN_REQUEST, USER_LOGOUT} from '../constants/userConstants'
+import { USER_LOGIN_FAIL, USER_LOGIN_SUCCESS, USER_LOGIN_REQUEST, USER_LOGOUT, USER_REGISTER_SUCCESS, USER_REGISTER_REQUEST, USER_REGISTER_FAIL} from '../constants/userConstants'
 
 export const loginUser = (email, password) => async (dispatch)=>{
     try {
@@ -24,6 +24,37 @@ export const loginUser = (email, password) => async (dispatch)=>{
     }catch (e) {
         dispatch({
             type: USER_LOGIN_FAIL,
+            payload: e.response &&  e.response.data.message ? e.response.data.message : e.message
+        })
+    }
+};
+
+export const registerUser = (email, password, name) => async (dispatch)=>{
+    try {
+        dispatch({
+            type:USER_REGISTER_REQUEST
+        })
+
+        const config = {
+            headers: {
+                'Content-Type':'application/json'
+            }
+        }
+
+        const {data} = await axios.post('/api/users', {email, password, name}, config)
+
+        dispatch({
+            type:USER_REGISTER_SUCCESS,
+            payload:data
+        })
+        dispatch({
+            type:USER_LOGIN_SUCCESS,
+            payload:data
+        })
+        localStorage.setItem('userInfo', JSON.stringify(data))
+    }catch (e) {
+        dispatch({
+            type: USER_REGISTER_FAIL,
             payload: e.response &&  e.response.data.message ? e.response.data.message : e.message
         })
     }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,13 +3,14 @@ import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 import {cartReducer} from './reducers/cartReducer'
-import {loginReducer} from './reducers/userReducer'
+import {loginReducer, registerReducer} from './reducers/userReducer'
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart:cartReducer,
-    userLogin:loginReducer
+    userLogin:loginReducer,
+    userRegister:registerReducer
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [];
