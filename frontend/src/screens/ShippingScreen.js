57467997f089aeac0749a7d3544ab14fd4cd49f4
atroian <andrii.troian@corezoid.com>
2021-06-30 22:39:07 +0300

    steppers component. payment method screen, action and reducer

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -2,6 +2,8 @@ import React, {useState} from 'react';
 import {Button,Form} from 'react-bootstrap';
 import {useDispatch, useSelector} from 'react-redux'
 import FormContainer from '../components/FormContainer'
+import CheckoutSteps from '../components/CheckoutSteps'
+
 import {saveShippingAddress} from '../actions/cartAction'
 
 const ShippingScreen = ({history}) => {
@@ -23,6 +25,7 @@ const ShippingScreen = ({history}) => {
 
     return (
         <FormContainer>
+            <CheckoutSteps step1 step2/>
             <h1>Shipping details</h1>
             <Form onSubmit={submitHandler}>
                 <Form.Group controlId='address'>
